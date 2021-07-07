d09f9d39c8e47be2c7c344cca89b7226a532c3b6
Divy Shah <38474504+divyhshah@users.noreply.github.com>
2021-07-07 14:38:26 +0530

    Add files via upload
    added new feature in anomaly detection

--- anomaly.py
+++ anomaly.py
@@ -20,7 +20,6 @@ def app():
     with st.sidebar.header('1. Upload your CSV data'):
         uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv", "xlsx", "xls"])
 
-    # Anomaly detection
     if uploaded_file is not None:
         @st.cache
         def load_csv():
@@ -31,23 +30,40 @@ def app():
             return csv
         df = load_csv()
 
-        s = setup(df, session_id=123, silent=True)
-        iforest = create_model('iforest', fraction=0.1)
-        iforest_results = assign_model(iforest)
-        anomaly = iforest_results[iforest_results['Anomaly'] == 1]
         st.header('**Input DataFrame Sample**')
         st.write(df.sample(20))
-        st.write('---')
-        st.header('**Data contains Anomaly**')
-        st.write(anomaly.head(10))
-        st.text(f"Total {anomaly.shape[0]} anomalies found")
 
-        towrite = io.BytesIO()
-        downloaded_file = anomaly.to_excel(towrite, encoding='utf-8', index=False, header=True)
-        towrite.seek(0)  # reset pointer
-        b64 = base64.b64encode(towrite.read()).decode()  # some strings
-        linko = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="anomaly.xlsx">Download excel file</a>'
-        st.markdown(linko, unsafe_allow_html=True)
+        # target column selection
+        df = df.select_dtypes('number')
+        columns = df.columns.tolist()
+        target_column = st.sidebar.selectbox('Select your target column:', columns)
+
+        # shortlist column based on correlation with taget
+        corr = df.corr()
+        cor_target = abs(corr[target_column])
+        relevan_cols = cor_target[cor_target>=0.2].index
+        df = df[relevan_cols]
+
+        # Anomaly detection
+        try:
+            s = setup(df, session_id=123, silent=True)
+            iforest = create_model('iforest', fraction=0.1)
+            iforest_results = assign_model(iforest)
+            anomaly = iforest_results[iforest_results['Anomaly'] == 1]
+            anomaly = anomaly.drop('Anomaly', axis=1)
+            st.write('---')
+            st.header('**Data contains Anomaly**')
+            st.write(anomaly.head(10))
+            st.text(f"Total {anomaly.shape[0]} anomalies found")
+            towrite = io.BytesIO()
+            downloaded_file = anomaly.to_excel(towrite, encoding='utf-8', index=False, header=True)
+            towrite.seek(0)  # reset pointer
+            b64 = base64.b64encode(towrite.read()).decode()  # some strings
+            linko = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="anomaly.xlsx">Download excel file</a>'
+            st.markdown(linko, unsafe_allow_html=True)
+        except:
+            st.markdown('<font color="red">Please select another target column</font>', unsafe_allow_html=True)
+
     else:
         st.info('Awaiting for CSV file to be uploaded.')
         if st.button('Press to use Example Dataset'):
